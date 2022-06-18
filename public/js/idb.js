283c997a592387ab4ce81c1bb29f52cf6a3e1a2b
KingAusti <austin@thehenrys.xyz>
2022-06-18 14:35:05 -0700

    finishing idb

--- public/js/idb.js
+++ public/js/idb.js
@@ -27,5 +27,26 @@ const saveRecord = record => {
 }
 
 function checkDatabase() {
-    
+    const transaction = db.transaction(['pending'], 'readWrite')
+    const store = transaction.objectStore('pending')
+    const getAll = store.getAll()
+    getAll.onsuccess = () =>{
+        if(getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(() => {
+                const transaction = db.transaction(['pending'], 'readWrite')
+                const store = transaction.objectStore('pending')
+                store.clear()
+            })
+        }
+    }
 }
+
+window.addEventListener('online', checkDatabase);

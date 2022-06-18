9e8e80f1022747226f6c9c37d22e822051e9de8b
KingAusti <austin@thehenrys.xyz>
2022-06-18 16:48:01 -0700

    adding img files to manifest and service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,7 +9,14 @@ const FILES_TO_CACHE = [
     './js/idb.js',
     './js/index.js',
     './manifest.json',
-    //all of those damn icons
+    'icon-72x72.png',
+    'icon-96x96.png',
+    'icon-128x128.png',
+    'icon-144x144.png',
+    'icon-152x152.png',
+    'icon-192x192.png',
+    'icon-384x384.png',
+    'icon-512x512.png'
 ]
 
 self.addEventListener('install', event => {
